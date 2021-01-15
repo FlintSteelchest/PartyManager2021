9d0acb99aa321131c604a792860474150b499768
FlintSteelchest <georgeoobrien@gmail.com>
2021-01-15 20:17:00 +0000

    Menu and quit functionality + bits and pieces

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 #Imports
 import pygame
+import pygame.event as pyev
+
+##global variables
+currentState="none"
 
 ###funtions
 
@@ -9,11 +13,44 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 240))
+    clock = pygame.time.Clock()
+    clock.tick(1)
+    menu()
     pygame.quit()
 
 ##menu
 def menu():
-    pass
+    currentState = "menu"
+    noOfOptions = 2
+    selectedEvent = 1
+    loopno=0
+    while currentState == 'menu':
+        for event in pygame.event.get():
+
+            if event.type == pygame.KEYDOWN:
+                print(event.key)
+                if event == pygame.K_DOWN: 
+                    selectedEvent -= 1
+                    if selectedEvent == 0:
+                        selectedEvent = noOfOptions
+                        print("down")
+                if event == pygame.K_UP:
+                    selectedEvent += 1
+                    if selectedEvent == noOfOptions+1:
+                        selectedEvent = 1
+                        print("up")
+                if event.key == 32:
+                    if selectedEvent == 1:
+                        currentState = "start"
+                        print("Start selected")
+                    if selectedEvent == 2:
+                        currentState = "quit"
+                        print("Quit selected")
+
+    if currentState=="start":
+        start()
+    if currentState=="quit":
+        quit()
 
 #start
 def start():
@@ -21,7 +58,7 @@ def start():
 
 #quit
 def quit():
-    pass
+    exit(0)
 
 ##prep phase
 def goPrepPhase():
@@ -38,7 +75,10 @@ class lineup:
     pass
 
 #characters
-class characters:
+class character:
+    pass
+
+def generateCharacter():
     pass
 
 #events
