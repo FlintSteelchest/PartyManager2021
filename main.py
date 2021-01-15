1610ea1645ec7c57101a4e9c21fd8de90ae86f40
FlintSteelchest <georgeoobrien@gmail.com>
2021-01-15 19:02:38 +0000

    basic functions

--- main.py
+++ main.py
@@ -2,3 +2,26 @@
 
 #Imports
 import Pygame
+
+
+###funtions
+
+##menu
+def menu():
+    skip
+
+#start
+def start():
+    skip
+
+#quit
+def quit():
+    skip
+
+##prep phase
+def goPrepPhase():
+    skip
+
+##adventure phase
+def goAdvPhase():
+    skip
